087b9c4a3b5f749d61b229311bb45d6779f8ba9e
Somon Hossain <somon12230327@gmail.com>
2019-12-11 20:13:22 +0600

    first commit

--- app/src/main/java/com/worldtechpoints/bd_english_gramar/Features/grammar/GrammarSubmitActivity.java
+++ app/src/main/java/com/worldtechpoints/bd_english_gramar/Features/grammar/GrammarSubmitActivity.java
@@ -3,6 +3,7 @@ package com.worldtechpoints.bd_english_gramar.Features.grammar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.worldtechpoints.bd_english_gramar.R;
 
@@ -13,6 +14,14 @@ public class GrammarSubmitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grammar_submit);
 
+
+
+
+
+
+    }
+
+    public void GrammarSubmit(View view) {
 
 
 
